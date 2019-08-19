dc30aa918a8f02c389d947e007d617ad246309e5
Alex <bugrimov_a@bk.ru>
2019-08-19 23:27:34 +0300

    (#1) Инициализация приложения

---
+++ src/main/java/dev/bug/api/data/mos/ApiDataMosApplication.java
@@ -0,0 +1,13 @@
+package dev.bug.api.data.mos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDataMosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDataMosApplication.class, args);
+    }
+
+}
