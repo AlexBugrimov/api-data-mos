4309a132ee6ee72fb219873e8ba5fe7bcd671067
Alex <bugrimov_a@bk.ru>
2019-08-25 21:24:56 +0300

    (#21) Создан сервис Category

---
+++ src/main/java/dev/bug/api/data/mos/services/category/CategoryService.java
@@ -0,0 +1,7 @@
+package dev.bug.api.data.mos.services.category;
+
+import dev.bug.api.data.mos.model.Category;
+import dev.bug.api.data.mos.services.Service;
+
+public interface CategoryService extends Service<Category> {
+}
---
+++ src/main/java/dev/bug/api/data/mos/services/category/CategoryServiceRepository.java
@@ -0,0 +1,71 @@
+package dev.bug.api.data.mos.services.category;
+
+import dev.bug.api.data.mos.exceptions.CategoryAlreadyExistsException;
+import dev.bug.api.data.mos.exceptions.CategoryNotExistsException;
+import dev.bug.api.data.mos.model.Category;
+import dev.bug.api.data.mos.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceRepository implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceRepository(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category getOneById(Long id) {
+        return categoryRepository
+                .findById(id)
+                .orElseThrow(CategoryNotExistsException::new);
+    }
+
+    @Override
+    public Category save(Category category) {
+        final Category savedCategory = category.getCategoryId() == null
+                ? category : getExistingCategory(category.getCategoryId());
+        return categoryRepository.save(savedCategory);
+    }
+
+    @Override
+    public Category update(Long id, Category updatedCategory) {
+        final Category oldCategory = getExistingCategory(id);
+        final Category newCategory = Category
+                .builder()
+                .categoryId(oldCategory.getCategoryId())
+                .name(updatedCategory.getName())
+                .englishName(updatedCategory.getEnglishName())
+                .description(updatedCategory.getDescription())
+                .englishDescription(updatedCategory.getEnglishDescription())
+                .icon(updatedCategory.getIcon())
+                .apps(updatedCategory.getApps())
+                .dataSets(updatedCategory.getDataSets())
+                .build();
+        return categoryRepository.save(newCategory);
+    }
+
+    @Override
+    public void delete(Long id) {
+        categoryRepository.deleteById(getExistingCategory(id).getCategoryId());
+    }
+
+    @Override
+    public void clean() {
+        categoryRepository.deleteAll();
+    }
+
+    private Category getExistingCategory(Long id) {
+        return categoryRepository.findById(id).orElseThrow(CategoryAlreadyExistsException::new);
+    }
+}
