0328c78d015b57af239869bb2b6c46042e3bd556
Alex <bugrimov_a@bk.ru>
2019-08-31 01:14:22 +0300

    (#32) Refactor ServiceApi

--- src/main/java/dev/bug/api/data/mos/services/ServiceApi.java
+++ src/main/java/dev/bug/api/data/mos/services/ServiceApi.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
@@ -20,26 +18,23 @@ public class ServiceApi {
     @Value("${host}")
     private String host;
 
-    @Value("${confidential-key}")
-    private String confidentialKey;
-
-    private RestTemplate template;
+    private final RestTemplate template;
+    @Value("${api_key}")
+    private String apiKey;
 
     @Autowired
     public ServiceApi(RestTemplate template) {
         this.template = template;
     }
 
-    public <T> T[] getItems(Class<T[]> type, Resource resource) {
-        final UriComponents uri = UriComponentsBuilder
-                .newInstance()
-                .scheme(scheme)
-                .host(host)
-                .path(resource.getPath())
-                .queryParam("api_key", confidentialKey)
-                .build();
-
-        final ResponseEntity<T[]> response = template.exchange(uri.toUriString(), HttpMethod.GET, null, type);
-        return response.getBody();
+    public <T> T[] getItems(final Class<T[]> type, final Resource resource) {
+        return template.exchange(UriComponentsBuilder
+                        .newInstance()
+                        .scheme(scheme)
+                        .host(host)
+                        .path(resource.getPath())
+                        .queryParam("api_key", apiKey)
+                        .build().toUriString(),
+                HttpMethod.GET, null, type).getBody();
     }
 }
